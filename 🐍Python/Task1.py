188fbdf136aa1c018ba65c94c27be9328ced728a
Lc22449 <lc22449@my.westlake.school.nz>
2025-04-29 15:09:54 +1200

    Python coding

---
+++ 🐍Python/Task1.py
@@ -0,0 +1,16 @@
+x = input("what is your name")
+
+score = 0
+
+if x == "Molly":
+    print("Hello")
+    score += 1    
+
+elif x == "Bob":
+    print("Go away")
+
+else:
+    print("Who are you?")
+
+print(f'Your score is {score}')
+
