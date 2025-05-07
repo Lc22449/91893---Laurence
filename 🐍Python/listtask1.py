e3c2a978ced6288c9851861fe6c0a3587586e411
Lc22449 <lc22449@my.westlake.school.nz>
2025-05-07 15:06:45 +1200

    Lists and stuff

--- 🐍Python/listtask1.py
+++ 🐍Python/listtask1.py
@@ -1,11 +1,7 @@
+codes = ["level", "power", "racecar", "deed", "fight", "meme", "run"]
+list = []
+for i in codes:
+    if i == i[::-1]:
+        list.append(i)
 
-def reverse_words_in_list(codes):
-    return [word[::-1] for word in codes]
-
-codes = ["level", "power", "deed", "fight", "meme", "run"]
-palindromes = reverse_words_in_list(codes)
-
-print(palindromes)
-
-
-
+print(list)
